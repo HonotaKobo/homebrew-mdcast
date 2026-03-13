ea776270eaff4798e679c0afdff610038158693d
HonotaKobo <216468035+HonotaKobo@users.noreply.github.com>
2026-03-13 13:52:33 +0900

    Add mdview cask

---
+++ Casks/mdview.rb
@@ -0,0 +1,15 @@
+cask "mdview" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "0.1.0"
+  sha256 arm: "e62c2d6e9f4280a3bbbf4cf13c0e86fa0abcafbfe591e56289d07387048438d4",
+         intel: "aee2bc2781fc7bddcdfbb57c17642bc63c1f259351a21d931ee1a46bd4b5a7a6"
+
+  url "https://github.com/HonotaKobo/mdview/releases/download/v#{version}/mdview_#{version}_#{arch}.dmg"
+  name "mdview"
+  desc "Markdown viewer for the AI age"
+  homepage "https://github.com/HonotaKobo/mdview"
+
+  app "mdview.app"
+  binary "#{appdir}/mdview.app/Contents/MacOS/mdview"
+end
