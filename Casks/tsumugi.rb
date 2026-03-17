96f682a72f73068d11b328b2299c4abf99f0616d
honota <gameweb.dat.macoto@gmail.com>
2026-03-17 13:13:46 +0900

    .dmg → .app.tar.gz

--- Casks/tsumugi.rb
+++ Casks/tsumugi.rb
@@ -5,7 +5,7 @@ cask "tsumugi" do
   sha256 arm: "91938e150542d10139ada88a937b62293f437024db4fd0488ac407af5f1cf8b4",
          intel: "7e4d632e9a6f42e2707a4569570c5378b286b655ad3c2decc8830875388822d1"
 
-  url "https://github.com/HonotaKobo/tsumugi/releases/download/v#{version}/tsumugi_#{version}_#{arch}.dmg"
+  url "https://github.com/HonotaKobo/tsumugi/releases/download/v#{version}/tsumugi_#{version}_#{arch}.app.tar.gz"
   name "tsumugi"
   desc "Markdown viewer for the AI age"
   homepage "https://github.com/HonotaKobo/tsumugi"
